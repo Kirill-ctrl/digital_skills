8e0766fc7a2ef445787008a60a3565d5a307a094
Kirill Pechurin <k.pechurin02@mail.ru>
2021-06-02 21:04:22 +0300

    week2 | edited working file system

--- week2/1_working_file_system.py
+++ week2/1_working_file_system.py
@@ -1,5 +1,4 @@
 import random
-import struct
 
 TEXT = """
 Lorem Ipsum - это текст-"рыба", часто используемый в печати и вэб-дизайне. Lorem Ipsum является стандартной "рыбой" 
@@ -12,16 +11,37 @@ PREP = ",.?!:;"
 
 ARR_DIGIT = [random.randint(0, 100) for _ in range(len(TEXT))]
 
-# не сделано
 
-with open('files_for_task_1/binary_file.txt', 'wb') as f:
-    for word, digit in list(zip(TEXT, ARR_DIGIT)):
-        string = word + " " + str(digit) + ' ' + str(random.choice(PREP))
-        string = f"{string.encode('utf-8')}" + "\n"
-        f.write(struct.pack("i", string))
+def create_binary_file():
+    """Заполняем файл бинарными данными"""
+    with open('files_for_task_1/binary_file.txt', 'wb') as f:
+        for word, digit in list(zip(TEXT, ARR_DIGIT)):
+            string = word + " " + str(digit) + ' ' + str(random.choice(PREP))
+            f.write(string.encode('utf-8'))
+            f.write('\n'.encode('utf-8'))
 
 
-with open('files_for_task_1/binary_file.txt', 'rb') as f:
-    data = f.read()
-    for lines in data:
-        print(struct.unpack("i", lines))
+def read_binary_file():
+    """Читаем файл и подсчитываем строки, слова, символы"""
+    counter_string = 0
+    counter_symb = 0
+    counter_lines = 0
+    with open('files_for_task_1/binary_file.txt', 'rb') as f:
+        for line in f.readlines():
+            counter_lines += 1
+            for obj in line.split():
+                decode_obj = obj.decode('utf-8')
+                if decode_obj in TEXT:
+                    counter_string += 1
+                    counter_symb += len(decode_obj.split())
+                if decode_obj in ARR_DIGIT or decode_obj in PREP:
+                    counter_symb += len(decode_obj.split())
+    return counter_string, counter_symb, counter_lines
+
+
+if __name__ == '__main__':
+    create_binary_file()
+    counter_string, counter_symb, counter_lines = read_binary_file()
+    print("count string: ", counter_string)
+    print("count symb: ", counter_symb)
+    print("count lines: ", counter_lines)

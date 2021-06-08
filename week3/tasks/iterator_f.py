257347aa0fd6c259dc5d07bcc421c8937b7cb10c
Kirill <k.pechurin02@mail.ru>
2021-06-08 17:26:09 +0300

    week3 | add iterator digits fibonacci

---
+++ week3/tasks/iterator_f.py
@@ -0,0 +1,21 @@
+class IteratorFibonacci:
+    def __init__(self, limit: int):
+        self.digit = 1
+        self.pr_digit = 0
+        self.limit = limit
+        self.counter = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.counter < self.limit:
+            self.counter += 1
+            self.pr_digit, self.digit = self.digit, self.pr_digit + self.digit
+            return self.digit
+        raise StopIteration
+
+
+f_iter_1 = IteratorFibonacci(100)
+for digit in f_iter_1:
+    print(digit)

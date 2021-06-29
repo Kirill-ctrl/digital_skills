4c4de80fc7e8b3e866c19fe961c99bc1eba6dc49
Kirill <k.pechurin02@mail.ru>
2021-06-29 21:57:17 +0300

    week6 | add services from base service

---
+++ week6/task_1_2/services/account.py
@@ -0,0 +1,30 @@
+import html
+import json
+import xml
+
+from course.week6.task_1_2.deserializers.account_des import AccountDeserializer
+from course.week6.task_1_2.formats import FORMAT_JSON, FORMAT_XML, FORMAT_HTML
+from course.week6.task_1_2.models.account import Account
+from course.week6.task_1_2.seializers.account_ser import AccountSerializer
+from course.week6.task_1_2.services.base import BaseF
+
+
+class AccountF(BaseF):
+
+    def serialize_to_json(self, account: Account):
+        return AccountSerializer.get_serialize(account, FORMAT_JSON)
+
+    def serialize_to_xml(self, account: Account):
+        return AccountSerializer.get_serialize(account, FORMAT_XML)
+
+    def serialize_to_html(self, account: Account):
+        return AccountSerializer.get_serialize(account, FORMAT_HTML)
+
+    def deserialize_from_json(self, json_obj: json):
+        return AccountDeserializer.get_deserialize(json_obj, FORMAT_JSON)
+
+    def deserialize_from_xml(self, xml_obj: xml):
+        return AccountDeserializer.get_deserialize(xml_obj, FORMAT_XML)
+
+    def deserialize_from_html(self, html_obj: html):
+        return AccountDeserializer.get_deserialize(html_obj, FORMAT_HTML)
---
+++ week6/task_1_2/services/news.py
@@ -0,0 +1,31 @@
+import html
+import json
+import xml
+
+from course.week6.task_1_2.deserializers.news_des import NewsDeserializer
+from course.week6.task_1_2.formats import FORMAT_JSON, FORMAT_XML, FORMAT_HTML
+
+from course.week6.task_1_2.models.news import News
+from course.week6.task_1_2.seializers.news_ser import NewsSerializer
+from course.week6.task_1_2.services.base import BaseF
+
+
+class NewsF(BaseF):
+
+    def serialize_to_json(self, news: News):
+        return NewsSerializer.get_serialize(news, FORMAT_JSON)
+
+    def serialize_to_xml(self, news: News):
+        return NewsSerializer.get_serialize(news, FORMAT_XML)
+
+    def serialize_to_html(self, news: News):
+        return NewsSerializer.get_serialize(news, FORMAT_HTML)
+
+    def deserialize_from_json(self, json_obj: json):
+        return NewsDeserializer.get_deserialize(json_obj, FORMAT_JSON)
+
+    def deserialize_from_xml(self, xml_obj: xml):
+        return NewsDeserializer.get_deserialize(xml_obj, FORMAT_XML)
+
+    def deserialize_from_html(self, html_obj: html):
+        return NewsDeserializer.get_deserialize(html_obj, FORMAT_HTML)

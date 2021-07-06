f12e7149441c910157cd2e2837357f2acb28f9ce
Kirill <k.pechurin02@mail.ru>
2021-07-06 21:03:23 +0300

    week7 | create connection through engine

---
+++ week7/connection/base_engine.py
@@ -0,0 +1,19 @@
+from sqlalchemy import create_engine
+from sqlalchemy.engine import Engine
+
+
+class EngineConn:
+
+    engine = None
+
+    @classmethod
+    def set_engine(cls, database: str) -> None:
+        cls.engine = create_engine(f'sqlite:///{database}.db', echo=True)
+
+    @classmethod
+    def get_engine(cls) -> Engine:
+        return cls.engine
+
+    @classmethod
+    def get_connect(cls):
+        return cls.engine.connect()

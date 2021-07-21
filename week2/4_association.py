cbf9a59b31ff6250abac0abc4c647513550a080b
Kirill <k.pechurin02@mail.ru>
2021-07-21 22:11:08 +0300

    week8 | delete rubish

--- week2/4_association.py
+++
@@ -1,5 +0,0 @@
-from functools import reduce
-
-if __name__ == '__main__':
-    # Соединяет слова, преобразуя в прописной вариант
-    print(reduce(lambda x, y: (x + ' ' + y).lower(), ['privet', 'mir', 'hello', 'world']))
